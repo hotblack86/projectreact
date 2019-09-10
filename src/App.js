e862faf7cd0ba8bdef3998bcaaec872ef787d89f
James Hotblack <32878691+hotblack86@users.noreply.github.com>
2019-09-10 18:30:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,8 +25,3 @@ function App(){
 }
 
 export default App;
-
-/* <Tweet name="James Hotblack" message="I am the one and only" likes="3"/>
-      <Tweet name="Stephen Fry" message="I am the one and only" likes="30,000"/>
-      <Tweet name="Sigmund Freud" message="I am the one and only" likes="50,000"/>
-      <Tweet name="Karl Marx" message="I am the one and only" likes="1,000,000"/> */
